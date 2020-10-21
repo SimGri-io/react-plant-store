11e0a6293918113f00909ac5ec925c62e413a6bd
Simona G <simonagribulyte@gmail.com>
2020-10-21 16:04:16 +0300

    front commit

--- src/context/cart.js
+++ src/context/cart.js
@@ -1,6 +1,5 @@
 // cart context
 import React, { useState, useEffect } from "react";
-import localCart from "../utils/localCart";
 
 function getCartFromLocalStorage() {
   return localStorage.getItem("cart")
